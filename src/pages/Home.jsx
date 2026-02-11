3af4f4555f82f13b20bfa6b6139aa7e4ad50fdb6
Muhammad Firdaus Hamid <tijarahmutiaratrading@gmail.com>
2026-02-11 05:51:35 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSection from "@/components/landing/HeroSection";
 import ProblemsSection from "@/components/landing/ProblemsSection";
-import SeriousProblemsSection from "@/components/landing/SeriousProblemsSection";
 import BenefitsSection from "@/components/landing/BenefitsSection";
+import VideoTestimonialSection from "@/components/landing/VideoTestimonialSection";
+import TestimonialsSection from "@/components/landing/TestimonialsSection";
 import IngredientsSection from "@/components/landing/IngredientsSection";
 import UsageSection from "@/components/landing/UsageSection";
-import DetailedUsageSection from "@/components/landing/DetailedUsageSection";
-import TestimonialsSection from "@/components/landing/TestimonialsSection";
-import VideoTestimonialSection from "@/components/landing/VideoTestimonialSection";
 import BeforeAfterSection from "@/components/landing/BeforeAfterSection";
-import ExpertEndorsementSection from "@/components/landing/ExpertEndorsementSection";
-import GuaranteeSection from "@/components/landing/GuaranteeSection";
-import UrgencySection from "@/components/landing/UrgencySection";
 import CertificateSection from "@/components/landing/CertificateSection";
+import GuaranteeSection from "@/components/landing/GuaranteeSection";
 import PricingSection from "@/components/landing/PricingSection";
 import FAQSection from "@/components/landing/FAQSection";
 import FooterSection from "@/components/landing/FooterSection";
 import ExportHTMLButton from "@/components/landing/ExportHTMLButton";
+import StickyCTA from "@/components/landing/StickyCTA";
+import CTABreak from "@/components/landing/CTABreak";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white" style={{ fontFamily: "'Inter', system-ui, sans-serif" }}>
       <HeroSection />
       <ProblemsSection />
-      <SeriousProblemsSection />
       <BenefitsSection />
+      <CTABreak />
+      <VideoTestimonialSection />
+      <TestimonialsSection />
       <IngredientsSection />
       <UsageSection />
-      <DetailedUsageSection />
-      <TestimonialsSection />
-      <VideoTestimonialSection />
       <BeforeAfterSection />
-      <ExpertEndorsementSection />
-      <GuaranteeSection />
-      <UrgencySection />
       <CertificateSection />
+      <CTABreak />
+      <GuaranteeSection />
       <PricingSection />
       <FAQSection />
       <FooterSection />
+      <StickyCTA />
       <ExportHTMLButton />
     </div>
   );
