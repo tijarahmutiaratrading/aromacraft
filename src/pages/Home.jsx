bd2615c5e977fddedcf424213bb00d3e0dae3ee3
Muhammad Firdaus Hamid <tijarahmutiaratrading@gmail.com>
2026-02-11 04:33:58 +0000

    File changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HeroSection from "@/components/landing/HeroSection";
+import ProblemsSection from "@/components/landing/ProblemsSection";
+import BenefitsSection from "@/components/landing/BenefitsSection";
+import IngredientsSection from "@/components/landing/IngredientsSection";
+import UsageSection from "@/components/landing/UsageSection";
+import TestimonialsSection from "@/components/landing/TestimonialsSection";
+import PricingSection from "@/components/landing/PricingSection";
+import FAQSection from "@/components/landing/FAQSection";
+import FooterSection from "@/components/landing/FooterSection";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white" style={{ fontFamily: "'Inter', system-ui, sans-serif" }}>
+      <HeroSection />
+      <ProblemsSection />
+      <BenefitsSection />
+      <IngredientsSection />
+      <UsageSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <FAQSection />
+      <FooterSection />
+    </div>
+  );
+}
